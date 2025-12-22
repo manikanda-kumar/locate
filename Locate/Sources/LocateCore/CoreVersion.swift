fc2be6b671fe4beba0e3b4365c01319eefe3cc9b
Manikanda Kumar <manikandakumar@gmail.com>
2025-12-22 21:05:47 +0530

    bump version to 1.1.0

--- Locate/Sources/LocateCore/CoreVersion.swift
+++ Locate/Sources/LocateCore/CoreVersion.swift
@@ -1,5 +1,5 @@
 public struct CoreVersion {
-    public static let string = "0.1.0"
+    public static let string = "1.1.0"
 
     public static func description() -> String {
         "LocateCore v\(string)"
